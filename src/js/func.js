45e8a79b72699f269e70558cc99c570f2537bc52
serebii <422574650@qq.com>
2018-04-08 17:16:59 +0800

    search page init

--- src/js/func.js
+++ src/js/func.js
@@ -16,6 +16,10 @@ var app = angular.module('myApp', ['ngRoute'])
             templateUrl: 'html/appointment.html',
             controller:'appointmentController'
         })
+        .when('/search',{
+            templateUrl:'html/search.html',
+            controller:'searchController'
+        })
         .otherwise({redirectTo:'/'});
 }]);
 /**
@@ -23,8 +27,6 @@ var app = angular.module('myApp', ['ngRoute'])
  */
 app.controller('indexController', function($scope,$timeout,$compile) {
     console.log('index');
-    $.init();
-
     $scope.arr = ['1','2','3'];
 
 
@@ -34,26 +36,50 @@ app.controller('indexController', function($scope,$timeout,$compile) {
         pagination : '.swiper-pagination',
         loop: true
     });
+
+    $.init();//放最后
 });
 /**
  * 医院页
  */
 app.controller('hospitalController',function($scope,$compile){
     console.log('hospital');
-    $.init();
 
-    $scope.stretchFlag = false;
-
-    $scope.toggleStretch = function (){
-        $scope.stretchFlag = !$scope.stretchFlag;
+    $scope.hospital = {
+        '医院简介':{
+            stretchFlag:false,
+            content:'某某市妇幼保健院是一所政府公立医院，同时也是某某市妇女儿童医院、某某市各县市区新农合定点医院、医保定点医院、某某医学院教学医院。医院拥有职工280余人（其中专业技术人员179人）、床位155张、各种大型医疗设备40余台件，是集医疗、保健、科研、教学为一体的现代化的三级乙等妇幼保健院。医院设临床部和保健部。临床部设有妇科、产科、儿科、新生儿科、无痛人流科、乳腺科、优生优育科、生殖保健科、内科、外科等10余个临床科室及B超室、放射科、输血科、药械科、检验科、心电图室等10余个医技科室，保健部设基层保健科、妇女保健科、儿童保健科等。 某某市妇幼保健院将始终坚持“患者至上，质量第一”的服务理念，秉承“低廉收费，优质服务”的办院宗旨，为广大市民朋友提供优质高效的医疗保健服务，为某某市妇幼保健事业做出更大的贡献。'
+        },
+        '医院荣誉':{
+            stretchFlag:false,
+            content:'1988年某某市政府命名为“某某市妇产医院”； 1995年获卫生部“二级甲等医院”和“爱婴医院”； 1999年医院荣获“省级文明单位”； 2000年荣获“某某省卫生行业文明示范”单位； 2002年获某某省首批“百姓放心医院”； 2005年获“某某省消费者放心医院”、“公安法医创伤医院”； 2006年获“某某省护理工作先进集体”； 2007年获“某某省先进集体”； 2007年获全省卫生系统“创佳评差”竞赛活动最佳单位 2009年荣获某某省诚信单位。 2010年被某某省中医管理局授予“三级甲等中西医结合医院” 2011年荣获“最受市民信赖的医疗机构”； 2011年荣获某某高校巾帼建功标兵岗。 2011年荣获某某市卫生工作先进单位。 2012年被国家中医药管理局授予“三级甲等中西医结合医院” 2014年～2015年被市卫生局评为“行风建设先进集体” 获全市卫生系统“创佳选优评差”竞赛活动先进单位 2016年被省教育工委评为“先进基层党组织”'
+        },
+        '医院特色':{
+            stretchFlag:false,
+            content:'医院现有国家级、省级重点建设专学科17个，省级二级实验室1个，国医堂、名老中医工作室5个，国家级师承导师、省级名老中医殷克敬教授工作室1个。重点专科和学科分布在心血管病、脑病、糖尿病、血液病、肿瘤、脾胃病、肺病、妇产科、重症医学、治未病、中医康复学、介入治疗、新生儿科、儿科、儿保科、乳腺科、骨伤骨病、心脏外科和泌尿外科等重大疑难疾病和社会急需的专业，并开设国医堂和名老中医工作室5个，名老中医传承工作室1个，包括国医大师张学文教授和郭诚杰教授等一批全国著名中医药专家长期座诊和查房。妇产学科在某某省医疗卫生系统中居于前列，是卫计委指定的全省危重孕产妇救治与转诊中心定点医院，被誉为本地区的“母婴健康的保护神”。'
+        }
     };
 
+    $scope.toggleStretch = function (){
+        this.val.stretchFlag = !this.val.stretchFlag;
+    };
+
+    $.init();//放最后
 });
 /**
  * 预约挂号页
  */
 app.controller('appointmentController',function($scope){
     console.log('appointment');
-    $.init();
 
+    $.init();//放最后
+});
+
+/**
+ * 医生搜索页面
+ */
+app.controller('searchController',function($scope){
+    console.log('search');
+
+    $.init();//放最后
 });
--- src/js/main.js
+++ src/js/main.js
@@ -57,3 +57,218 @@ function zmodel(text,title,buttons){
         buttons: buttons
     })
 }
+
+function getParam(name){
+    var reg = new RegExp("[^\?&]?" + encodeURI(name) + "=[^&]+");
+    var arr = location.hash.substring(1).match(reg);
+    if (arr != null) {
+        return decodeURI(arr[0].substring(arr[0].search("=") + 1));
+    }
+    return "";
+}
+
+function getRandomNum(Min,Max){
+    var Range = Max - Min;
+    var Rand = Math.random();
+    return(Min + Math.round(Rand * Range));
+}
+
+//字符函数: startWith
+String.prototype.startWith = function(str){
+    var reg=new RegExp("^"+str);
+    return reg.test(this);
+};
+
+//字符函数: endWith
+String.prototype.endWith=function(str){
+    var reg=new RegExp(str+"$");
+    return reg.test(this);
+};
+
+//字符函数: 全替换
+String.prototype.replaceAll = function(s1, s2){
+    return this.replace(new RegExp(s1,"gm"),s2);
+};
+
+function isValidDate(iY, iM, iD) {
+    var a=new Date(iY,iM-1,iD);
+    var y=a.getFullYear();
+    var m=a.getMonth()+1;
+    var d=a.getDate();
+    if (y!=iY || m!=iM || d!=iD)
+        return false;
+    return true
+}
+
+function isNum(str){
+    if(str == "")
+        return false;
+    return !isNaN(str);
+}
+
+function isMobile(s){
+    var patrn=/^1[0-9]{10}$/;
+    if(!patrn.exec(s))
+        return false;
+    return true;
+}
+
+function isInteger(str) {
+    if(/[^\d]+$/.test(str)){
+        return false;
+    }
+    return true;
+}
+
+function isNum(str){
+    if(str == "")
+        return false;
+    return !isNaN(str);
+}
+
+function isIDCard(StrNo){
+    StrNo = StrNo.toString();
+    if(StrNo.length==18){
+        var a,b,c;
+        if(!isInteger(StrNo.substr(0,17)))
+            return false;
+
+        if(!isValidDate(StrNo.substr(6,4),StrNo.substr(10,2),StrNo.substr(12,2)))
+            return false;
+
+        a=parseInt(StrNo.substr(0,1))*7+parseInt(StrNo.substr(1,1))*9+parseInt(StrNo.substr(2,1))*10;
+        a=a+parseInt(StrNo.substr(3,1))*5+parseInt(StrNo.substr(4,1))*8+parseInt(StrNo.substr(5,1))*4;
+        a=a+parseInt(StrNo.substr(6,1))*2+parseInt(StrNo.substr(7,1))*1+parseInt(StrNo.substr(8,1))*6;
+        a=a+parseInt(StrNo.substr(9,1))*3+parseInt(StrNo.substr(10,1))*7+parseInt(StrNo.substr(11,1))*9;
+        a=a+parseInt(StrNo.substr(12,1))*10+parseInt(StrNo.substr(13,1))*5+parseInt(StrNo.substr(14,1))*8;
+        a=a+parseInt(StrNo.substr(15,1))*4+parseInt(StrNo.substr(16,1))*2;
+        b=a%11;
+
+        if(b==2)
+            c=StrNo.substr(17,1).toUpperCase(); //转为大写X
+        else
+            c=parseInt(StrNo.substr(17,1));
+
+        switch(b){
+            case 0:
+                if(c!=1)
+                    return false;
+                break;
+            case 1:
+                if(c!=0)
+                    return false;
+                break;
+            case 2:
+                if(c!="X")
+                    return false;
+                break;
+            case 3:
+                if(c!=9)
+                    return false;
+                break;
+            case 4:
+                if(c!=8)
+                    return false;
+                break;
+            case 5:
+                if(c!=7)
+                    return false;
+                break;
+            case 6:
+                if(c!=6)
+                    return false;
+                break;
+            case 7:
+                if(c!=5)
+                    return false;
+                break;
+            case 8:
+                if(c!=4)
+                    return false;
+                break;
+            case 9:
+                if(c!=3)
+                    return false;
+                break;
+            case 10:
+                if(c!=2)
+                    return false;
+        }
+    }else{
+        return false;
+    }
+
+    return true;
+}
+
+function isCorrectName(name){
+    var regEn = /[`~!@#$%^&*()_+<>?:"{},.\/;'[\]]/im,
+        regCn = /[·！#￥（——）：；“”‘、，|《。》？、【】[\]]/im,
+        len = 0;
+    //判断字符串字符长度
+    for (var i = 0; i < name.length; i++) {
+        var a = name.charAt(i);
+        if (a.match(/[^\x00-\xff]/ig) != null)
+        {
+            len += 2;
+        }
+        else
+        {
+            len += 1;
+        }
+    }
+    if(regEn.test(name) || regCn.test(name)) {
+        return false;
+    }
+    if(len > 20) {
+        return false
+    }
+    return true;
+}
+
+//获取某年某月的天数
+function getDays(year,month){
+    return 33 - new Date(year,month-1,33).getDate();
+}
+
+//获取某天是星期几
+function getWeek(year,month,day){
+    var week = new Date(year,month-1,day).getDay();
+    if(week == 0) week = 7;
+    return week;
+}
+
+Date.prototype.format=function(format){
+    var o = {
+        "M+" : this.getMonth()+1, //month
+        "d+" : this.getDate(),    //day
+        "h+" : this.getHours(),   //hour
+        "m+" : this.getMinutes(), //minute
+        "s+" : this.getSeconds(), //second
+        "q+" : Math.floor((this.getMonth()+3)/3),  //quarter
+        "S" : this.getMilliseconds() //millisecond
+    };
+    if(/(y+)/.test(format)) format=format.replace(RegExp.$1,(this.getFullYear()+"").substr(4-RegExp.$1.length));
+    for(var k in o)if(new RegExp("("+ k +")").test(format))
+        format = format.replace(RegExp.$1,RegExp.$1.length==1?o[k]:("00"+ o[k]).substr((""+ o[k]).length));
+    return format;
+};
+
+//function formatTime(value){
+//    value = value.replaceAll("-","/").replace(".0","");
+//    return new Date(value);
+//}
+
+//function getDate(){
+//    var d = new Date();
+//    //return formatTime(d.format("yyyy-MM-dd") + " 00:00:00");
+//    return d.format("yyyy-MM-dd") + " 00:00:00";
+//}
+
+function getJson(obj){
+    return JSON.stringify(obj);
+}
+
+function getObj(str){
+    return JSON.parse(str);
+}
